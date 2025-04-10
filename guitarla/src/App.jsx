b6b7ab2d13bd3dcdc41deb284acce72422a0d8cf
Pilar González-Farret <pilar.gonzalezfarret@gmail.com>
2025-04-10 19:13:57 -0400

    update

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -6,17 +6,36 @@ import { db } from "./data/db"
 function App() {
 
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id )
+    if(itemExists >= 0) { //Existe en el carrito  
+      console.log("Ya existe");
+    } else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+    
+    
+  }
   
     return (
     <>
-
     <Header/>
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map(() => ( <Guitar></Guitar> ))}
+            {data.map((guitar) => ( 
+              <Guitar
+                key={guitar.id}
+                guitar={guitar}
+                setCart={setCart}
+                addToCart={addToCart}
+              /> 
+              ))}
             
         </div>
     </main>
