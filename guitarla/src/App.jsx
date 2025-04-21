78b3f5bb26e3496272d4807e8c9989b8e0007fd0
Pilar González-Farret <pilar.gonzalezfarret@gmail.com>
2025-04-21 19:54:05 -0400

    updated
    Aumentar y eliminar items del carrito

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -7,10 +7,14 @@ function App() {
 
   const [data, setData] = useState(db)
   const [cart, setCart] = useState([])
+  
+  const MIN_ITEMS = 1 
+  const MAX_ITEMS = 5
 
   function addToCart(item) {
     const itemExists = cart.findIndex(guitar => guitar.id === item.id )
     if(itemExists >= 0) { //Existe en el carrito  
+      if (cart[itemExists].quantity >= MAX_ITEMS ) return
       const updatedCart = [...cart]
       updatedCart[itemExists].quantity++
       setCart(updatedCart)
@@ -22,14 +26,46 @@ function App() {
 
   function removeFromCart(id) {
     setCart(prevCart => prevCart.filter(guitar => guitar.id !== id) )
-    
   }
   
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+  setCart(updatedCart)
+  }
+  
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
+
     return (
     <>
     <Header
       cart={cart}
       removeFromCart={removeFromCart}
+      increaseQuantity={increaseQuantity}
+      decreaseQuantity={decreaseQuantity}
+      clearCart={clearCart}
     />
     
 
--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react"
 
-export default function Header({cart, removeFromCart}) {
+export default function Header({cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart}) {
 
     //State derivado
     const isEmpty = useMemo(() => cart.length === 0, [cart])
@@ -54,6 +54,7 @@ export default function Header({cart, removeFromCart}) {
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
+                                                        onClick={() => decreaseQuantity(guitar.id) }
                                                     >
                                                         -
                                                     </button>
@@ -61,6 +62,7 @@ export default function Header({cart, removeFromCart}) {
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
+                                                        onClick={() => increaseQuantity(guitar.id)}
                                                     >
                                                         +
                                                     </button>
@@ -83,7 +85,9 @@ export default function Header({cart, removeFromCart}) {
                             )}
 
 
-                            <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                            <button className="btn btn-dark w-100 mt-3 p-2"
+                            onClick={clearCart}
+                            >Vaciar Carrito</button>
 
 
                         </div>
